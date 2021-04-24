38804051ecfe6036c04bce87f525ed6aaef46383
Yuriy Cherkashin <cherkashin.ju@gmail.com>
2021-04-24 23:16:06 +0300

    chore(vuetify-learning): Figured out about $vuetify.breakpoint

--- src/views/ViewMain.vue
+++ src/views/ViewMain.vue
@@ -1,5 +1,39 @@
 <template>
-  <layout-main :navigation="navigation" />
+  <layout-main :navigation="navigation">
+    <p>$vuetify.breakpoint</p>
+    <p>
+      <a
+        href="https://vuetifyjs.com/en/features/breakpoints/#breakpoint-service-object"
+      >
+        Docs
+      </a>
+    </p>
+    <pre>
+      name: {{ $vuetify.breakpoint.name }}
+      mobile: {{ $vuetify.breakpoint.mobile }}
+      mobileBreakpoint: {{ $vuetify.breakpoint.mobileBreakpoint }}
+      thresholds: {{ $vuetify.breakpoint.thresholds }}
+      height: {{ $vuetify.breakpoint.height }}
+      width: {{ $vuetify.breakpoint.width }}
+      scrollBarWidth: {{ $vuetify.breakpoint.scrollBarWidth }}
+      xl: {{ $vuetify.breakpoint.xl }}
+      xl: {{ $vuetify.breakpoint.xl }}
+      lg: {{ $vuetify.breakpoint.lg }}
+      lgAndDown: {{ $vuetify.breakpoint.lgAndDown }}
+      lgAndUp: {{ $vuetify.breakpoint.lgAndUp }}
+      lgOnly: {{ $vuetify.breakpoint.lgOnly }}
+      md: {{ $vuetify.breakpoint.md }}
+      mdAndDown: {{ $vuetify.breakpoint.mdAndDown }}
+      mdAndUp: {{ $vuetify.breakpoint.mdAndUp }}
+      mdOnly: {{ $vuetify.breakpoint.mdOnly }}
+      sm: {{ $vuetify.breakpoint.sm }}
+      smAndDown: {{ $vuetify.breakpoint.smAndDown }}
+      smAndUp: {{ $vuetify.breakpoint.smAndUp }}
+      smOnly: {{ $vuetify.breakpoint.smOnly }}
+      xs: {{ $vuetify.breakpoint.xs }}
+      xsOnly: {{ $vuetify.breakpoint.xsOnly }}
+    </pre>
+  </layout-main>
 </template>
 
 <script lang="ts">
@@ -14,5 +48,7 @@ import LayoutMain from '@/layout/LayoutMain.vue';
 })
 export default class ViewNotesList extends Vue {
   public navigation = ['Dashboard', 'Messages', 'Profile', 'Updates'];
+
+  public foo = this.$vuetify;
 }
 </script>
